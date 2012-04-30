240ed122a4561c772fcb1f35b380610909edc278
Bogdan Cristea <cristeab@gmail.com>
2012-04-30 22:30:41 +0200

    corrected unit test

--- test/main.cpp
+++ test/main.cpp
@@ -1,11 +1,14 @@
 #include <main.h>
 #include <okulardocument.h>
+#include <QApplication>
 
 #define PDF_FILE "/home/bogdan/Biblio/eBooks/umbrello.pdf"
 
 
 void TestTabletReader::openPDF()
 {
+	int argc = 0;
+	QApplication app(argc, NULL);
 	OkularDocument doc;
 	QCOMPARE(EXIT_SUCCESS, doc.load(PDF_FILE));
 	QVERIFY(NULL != doc.getPixmap(0, -1,  -1));
