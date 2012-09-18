3a63de1ddb9e8d37215ed1bc96384abf73bc9a18
Bogdan Cristea <cristeab@gmail.com>
2012-09-18 21:42:39 +0200

    cor cmake

--- cmake/Modules/FindLibOkularCore.cmake
+++ cmake/Modules/FindLibOkularCore.cmake
@@ -3,7 +3,9 @@
 find_path ( OKULAR_CORE_INCLUDE_DIR okular/core/document.h okular/core/page.h okular/core/generator.h)
 find_library ( OKULAR_CORE_LIBRARY NAMES okularcore )
 
-IF( NOT("${OKULAR_CORE_INCLUDE_DIR}" STREQUAL "") AND NOT("${OKULAR_CORE_LIBRARY}" STREQUAL "") )	
-	message("-- Found Okular core library")
+IF( (OKULAR_CORE_INCLUDE_DIR) AND (OKULAR_CORE_LIBRARY) )	
+	message(STATUS "Found Okular core library")
+else()
+	message(WARNING "Cannot find Okular core library")
 ENDIF()
 
