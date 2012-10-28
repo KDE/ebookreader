f82ed5ef5571f208851b4899e77ee62a41fd4cf4
Bogdan Cristea <cristeab@gmail.com>
2012-10-28 21:29:17 +0100

    corrected cmake

--- src/tabletReader_fr.ts
+++ src/tabletReader_fr.ts
@@ -28,183 +28,183 @@
 <context>
     <name>Window</name>
     <message>
-        <location filename="window.cpp" line="198"/>
-        <location filename="window.cpp" line="449"/>
+        <location filename="window.cpp" line="197"/>
+        <location filename="window.cpp" line="448"/>
         <source>Open</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="201"/>
+        <location filename="window.cpp" line="200"/>
         <source>Full Screen</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="204"/>
-        <location filename="window.cpp" line="452"/>
+        <location filename="window.cpp" line="203"/>
+        <location filename="window.cpp" line="451"/>
         <source>Go To Page</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="207"/>
-        <location filename="window.cpp" line="458"/>
+        <location filename="window.cpp" line="206"/>
+        <location filename="window.cpp" line="457"/>
         <source>Zoom</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="210"/>
-        <location filename="window.cpp" line="455"/>
+        <location filename="window.cpp" line="209"/>
+        <location filename="window.cpp" line="454"/>
         <source>Properties</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
+        <location filename="window.cpp" line="212"/>
         <location filename="window.cpp" line="213"/>
-        <location filename="window.cpp" line="214"/>
         <source>Help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="213"/>
+        <location filename="window.cpp" line="212"/>
         <source>Back</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="219"/>
+        <location filename="window.cpp" line="218"/>
         <source>About</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="222"/>
-        <location filename="window.cpp" line="461"/>
+        <location filename="window.cpp" line="221"/>
+        <location filename="window.cpp" line="460"/>
         <source>Quit</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="241"/>
+        <location filename="window.cpp" line="240"/>
         <source>Cannot create fileBrowser object</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="242"/>
+        <location filename="window.cpp" line="241"/>
         <source>not enough memory</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="246"/>
+        <location filename="window.cpp" line="245"/>
         <source>fileBrowserObject is NULL</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="464"/>
+        <location filename="window.cpp" line="463"/>
         <source>Normal Screen</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="493"/>
+        <location filename="window.cpp" line="492"/>
         <source>Failed to open file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="494"/>
+        <location filename="window.cpp" line="493"/>
         <source>%1 cannot be opened</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="839"/>
+        <location filename="window.cpp" line="838"/>
         <source>Cannot open help file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="860"/>
+        <location filename="window.cpp" line="859"/>
         <source>&lt;H2&gt;tabletReader v%1&lt;/H2&gt;&lt;H3&gt;e-book reader for touch-enabled devices&lt;/H3&gt;&lt;H4&gt;Supported formats:&lt;/H4&gt;&lt;H4&gt;%2&lt;/H4&gt;&lt;br&gt;Copyright (C) 2012, Bogdan Cristea. All rights reserved.&lt;br&gt;&lt;i&gt;e-mail: cristeab@gmail.com&lt;/i&gt;&lt;br&gt;&lt;br&gt;This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.&lt;br&gt;&lt;br&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="971"/>
+        <location filename="window.cpp" line="970"/>
         <source>Cannot get authorization code for Intel AppUp(TM) software</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="972"/>
+        <location filename="window.cpp" line="971"/>
         <source>You can use tabletReader, but it is highly recommended to connect to Intel AppUp center</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="995"/>
+        <location filename="window.cpp" line="994"/>
         <source>&lt;H3&gt;Document path:&lt;br&gt;&lt;i&gt;%1&lt;/i&gt;&lt;/H3&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="997"/>
+        <location filename="window.cpp" line="996"/>
         <source>&lt;H3&gt;Current page / Number of pages:&lt;br&gt;&lt;b&gt;%1 / %2&lt;/b&gt;&lt;/H3&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="999"/>
+        <location filename="window.cpp" line="998"/>
         <source>&lt;H3&gt;Elapsed time:&lt;br&gt;%1&lt;/H3&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="1002"/>
+        <location filename="window.cpp" line="1001"/>
         <source>&lt;H3&gt;Battery status:&lt;br&gt;%1&lt;/H3&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="1038"/>
+        <location filename="window.cpp" line="1037"/>
         <source>no charger</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="1041"/>
+        <location filename="window.cpp" line="1040"/>
         <source>wall charger</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="1047"/>
+        <location filename="window.cpp" line="1046"/>
         <source>charging</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="1051"/>
+        <location filename="window.cpp" line="1050"/>
         <source>unknown</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="1055"/>
+        <location filename="window.cpp" line="1054"/>
         <source>, %1% remaining capacity</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="1058"/>
+        <location filename="window.cpp" line="1057"/>
         <source>, unknown remaining capacity</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="1071"/>
+        <location filename="window.cpp" line="1070"/>
         <source>1 hour</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="1074"/>
+        <location filename="window.cpp" line="1073"/>
         <source>%1 hours</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="1083"/>
+        <location filename="window.cpp" line="1082"/>
         <source>1 minute</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="1086"/>
+        <location filename="window.cpp" line="1085"/>
         <source>%1 minutes</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="1095"/>
+        <location filename="window.cpp" line="1094"/>
         <source>1 second</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="window.cpp" line="1098"/>
+        <location filename="window.cpp" line="1097"/>
         <source>%1 seconds</source>
         <translation type="unfinished"></translation>
     </message>
